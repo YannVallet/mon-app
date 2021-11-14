f8bc7943e534110eb5d4eaa285357a8c9f9c3af1
Yann vallet <94015736+YannVallet@users.noreply.github.com>
2021-11-14 09:11:38 +0100

    Update FormInputs.js

--- src/FormInputs.js
+++ src/FormInputs.js
@@ -4,7 +4,47 @@ export default class FormInputs extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <div className="card my-3">
+          <div className="card-header">
+            Ajouter une personne
+            <div className="card-body">
+              <form>
+                <div className="form-group">
+                  <label htmlFor="nom">Nom</label>
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    name="nom"
+                  ></input>
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="age">Age</label>
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    name="age"
+                  ></input>
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="ville">Ville</label>
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    name="ville"
+                  ></input>
+                </div>
+
+                <div className="d-grid gap-2">
+                  <button className="btn btn-primary" type="button">
+                    Créez votre fiche !
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
