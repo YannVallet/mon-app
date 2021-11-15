cac7a31fc79bbe05fe2cc64b2c2dbc7aae2089fd
Yann vallet <94015736+YannVallet@users.noreply.github.com>
2021-11-15 20:41:08 +0100

    Create page1.js

---
+++ src/page1.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const element = <h1>Bonjour, monde</h1>;
+
+export default class page1 extends Component {
+  render() {
+    return <div></div>;
+  }
+}
