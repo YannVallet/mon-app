8750bd3cfadf9bfe0853eaccd7de8cf52312d49b
Yann vallet <94015736+YannVallet@users.noreply.github.com>
2021-11-15 20:41:02 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FormInputs from "./FormInputs";
+import page1 from "./page1";
 
 class App extends Component {
   render() {
